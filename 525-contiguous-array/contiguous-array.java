e9f77d580ba29985fa0dd1e52f414e2fb83c295c
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-03-19 10:49:34 +0530

    Time: 20 ms (87.68%) | Memory: 55.2 MB (27.37%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -1,7 +1,8 @@
 class Solution {
     public int findMaxLength(int[] nums) {
         HashMap<Integer, Integer> hm = new HashMap<>();
-        hm.put(0, -1); /// base case
+        hm.put(0, -1);
+        int ans = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
                 nums[i] = -1;
@@ -10,19 +11,18 @@ class Solution {
         int[] pf = new int[nums.length];
         pf[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            pf[i] = nums[i] + pf[i - 1];
+            pf[i] = pf[i - 1] + nums[i];
         }
-        int ans = 0;
         for (int i = 0; i < pf.length; i++) {
+            int current = 0;
             if (hm.containsKey(pf[i])) {
-                ans = Math.max(ans,i - hm.get(pf[i]));
-
+                current = i - hm.get(pf[i]);
+                ans = Math.max(ans, current);
             } else {
                 hm.put(pf[i], i);
             }
         }
-
-        return ans;
+        return ans==Integer.MIN_VALUE?0:ans;
 
     }
 }

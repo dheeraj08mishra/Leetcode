e3bbc5b53c4122460103ce603c72ce21da9ea9b2
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-03-22 19:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (22.04%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int ans = Integer.MIN_VALUE;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for(int j=0;j<accounts[0].length;j++){
+                sum = sum+accounts[i][j];
+            }
+            ans = Math.max(ans,sum);
+        }
+        return ans;
+    }
+}

a90073e9356a62623a532af12015661d8895a9f6
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-03-26 14:27:22 +0530

    Time: 6 ms (80.96%) | Memory: 44.7 MB (11.96%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,21 +1,43 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int p1 = 0, p2 = 1;
-        int count = 0;
+
+        /// Brute force
+        // int p1 = 0, p2 = 1;
+        // int count = 0;
+        // int[] arr = new int[nums.length];
+        // while (p1 < nums.length && p2 < nums.length) {
+        // if (nums[p1] != nums[p2] && nums[p2] < nums[p1]) {
+        // count++;
+        // p2++;
+        // } else {
+        // p2++;
+        // }
+        // arr[p1] = count;
+        // if (p2 == nums.length) {
+        // p1++;
+        // p2 = 0;
+        // count = 0;
+        // }
+        // }
+
+        /// hashmap approch
+
         int[] arr = new int[nums.length];
-        while (p1 < nums.length && p2 < nums.length) {
-            if (nums[p1] != nums[p2] && nums[p2] < nums[p1]) {
-                count++;
-                p2++;
+        int[] temp = new int[nums.length];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = nums[i];
+        }
+        Arrays.sort(temp); // 1,2,2,3,8
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < temp.length; i++) {
+            if (hm.containsKey(temp[i])) {
+                hm.put(temp[i], hm.get(temp[i]));
             } else {
-                p2++;
+                hm.put(temp[i], i);
             }
-            arr[p1] = count;
-            if (p2 == nums.length) {
-                p1++;
-                p2 = 0;
-                count = 0;
-            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = hm.get(nums[i]);
         }
         return arr;
     }

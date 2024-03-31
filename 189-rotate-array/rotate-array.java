ef345c0dce537e8067521c93ad1496daf94e3e57
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-03-31 13:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 57.7 MB (12.17%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,18 +1,16 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k=k%nums.length;
-        if(k>= nums.length){
-            return;
-        }
-         revseCode(nums,0,nums.length-1);
-        revseCode(nums,0,k-1);
-        revseCode(nums,k,nums.length-1);
-        }
-     private static void revseCode(int[] nums, int start, int end) {
-        while (start<end){
-            int temp=nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private static void reverse(int[] array, int start, int end) {
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
             start++;
             end--;
         }

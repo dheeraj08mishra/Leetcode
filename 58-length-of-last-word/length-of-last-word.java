e87d7495bfa1e2e6f1abaff81a9c3acd66e7be88
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-04-01 08:28:33 +0530

    Time: 3 ms (13.54%) | Memory: 41.3 MB (92.89%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int len = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                len = 0;
+            } else if (s.charAt(i) != ' ') {
+                len++;
+            }
+        }
+        return len;
+    }
+}

25a6d5a8db79f27bd2a33f499bac628ef4a54878
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-04-03 10:59:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (11.18%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // brute force linear search
+        int ans = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                ans = i;
+                break;
+            }
+
+        }
+        return ans;
+
+    }
+}

2b72ab25696f961500eeebd4d6ec3109added1ea
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-04-17 22:32:07 +0530

    Time: 67 ms (32.55%) | Memory: 49.5 MB (47.47%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,8 +4,9 @@
  * @return {Promise}
  */
 var addTwoPromises = async function (promise1, promise2) {
-    let p1 = await promise1;
-    let p2 = await promise2;
+    // let p1 = await promise1;
+    // let p2 = await promise2;
+    let [p1, p2] = await Promise.all([promise1, promise2]);
     return p1 + p2;
 };
 

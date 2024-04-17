6e541fe9eefd15330783c867af66b513a8d7795f
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-04-17 17:15:21 +0530

    Time: 49 ms (5.07%) | Memory: 44.2 MB (5.06%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,43 +1,60 @@
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
-                stack.push(s.charAt(i));
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack = new Stack<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
+//                 stack.push(s.charAt(i));
+//             } else {
+//                 if (stack.isEmpty()) {
+//                     if (s.charAt(i) == '}' || s.charAt(i) == ')' || s.charAt(i) == ']') {
+//                         return false;
+//                     }
+//                 }
+//                 if (s.charAt(i) == '}') {
+//                     if (stack.peek() == '{') {
+//                         stack.pop();
+//                     } else {
+//                         stack.push(s.charAt(i));
+//                     }
+//                 }
+//                 if (s.charAt(i) == ')') {
+//                     if (stack.peek() == '(') {
+//                         stack.pop();
+//                     } else {
+//                         stack.push(s.charAt(i));
+//                     }
+//                 }
+//                 if (s.charAt(i) == ']') {
+//                     if (stack.peek() == '[') {
+//                         stack.pop();
+//                     } else {
+//                         stack.push(s.charAt(i));
+//                     }
+//                 }
+//             }
+//         }
+//         if (!stack.isEmpty()) {
+//             return false;
+//         } else {
+//             return true;
+//         }
+
+//     }
+// }
+
+public class Solution {
+    public static boolean isValid(String s) {
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
             } else {
-                if (stack.isEmpty()) {
-                    if (s.charAt(i) == '}' || s.charAt(i) == ')' || s.charAt(i) == ']') {
-                        return false;
-                    }
-                }
-                if (s.charAt(i) == '}') {
-                    if (stack.peek() == '{') {
-                        stack.pop();
-                    } else {
-                        stack.push(s.charAt(i));
-                    }
-                }
-                if (s.charAt(i) == ')') {
-                    if (stack.peek() == '(') {
-                        stack.pop();
-                    } else {
-                        stack.push(s.charAt(i));
-                    }
-                }
-                if (s.charAt(i) == ']') {
-                    if (stack.peek() == '[') {
-                        stack.pop();
-                    } else {
-                        stack.push(s.charAt(i));
-                    }
-                }
+                // If the string becomes empty, it indicates all brackets are matched.
+                return s.isEmpty();
             }
         }
-        if (!stack.isEmpty()) {
-            return false;
-        } else {
-            return true;
-        }
-
     }
 }

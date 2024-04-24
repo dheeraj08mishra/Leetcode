b4ed80ce102df90adb24931d40e96326f61cca76
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-04-24 15:35:22 +0530

    Time: 5 ms (82.66%) | Memory: 68 MB (37.47%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,29 +1,23 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        ArrayList<Integer> lesser = new ArrayList<>();
-        ArrayList<Integer> equal = new ArrayList<>();
-        ArrayList<Integer> greater = new ArrayList<>();
+        int counter = 0;
+        int[] ans = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] < pivot) {
-                lesser.add(nums[i]);
-            } else if (nums[i] > pivot) {
-                greater.add(nums[i]);
-            } else {
-                equal.add(nums[i]);
+                ans[counter++] = nums[i];
             }
         }
-        int[] ans = new int[nums.length];
-        int counter = 0;
-        for (int i = 0; i < lesser.size(); i++) {
-            ans[counter++] = lesser.get(i);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == pivot) {
+                ans[counter++] = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > pivot) {
+                ans[counter++] = nums[i];
+            }
+        }
 
-        }
-        for (int i = 0; i < equal.size(); i++) {
-            ans[counter++] = equal.get(i);
-        }
-        for (int i = 0; i < greater.size(); i++) {
-            ans[counter++] = greater.get(i);
-        }
         return ans;
     }
 }

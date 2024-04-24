a35af52d73570ccd6d58ae8e120b2c21d0ecd78c
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-04-24 17:53:49 +0530

    Time: 16 ms (66.67%) | Memory: 57.6 MB (50.93%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -1,20 +1,19 @@
 class Solution {
     public long dividePlayers(int[] skill) {
         Arrays.sort(skill);
-        int pointer1=0,pointer2=skill.length-1;
-        int currentSum = skill[pointer1]+skill[pointer2];
-        for(int i=0;i<skill.length;i++){
-            if(skill[pointer1]+skill[pointer2]!=currentSum){
+        int pointer1 = 0, pointer2 = skill.length - 1;
+        int currentSum = skill[pointer1] + skill[pointer2];
+        for (int i = 0; i < skill.length; i++) {
+            if (skill[i] + skill[skill.length - 1 - i] != currentSum) {
                 return -1;
             }
-            pointer1++;
-            pointer2--;
+
         }
-        long ans=0;
-         pointer1=0;
-        pointer2=skill.length-1;
-        while(pointer1<=pointer2){
-            ans+=skill[pointer1]*skill[pointer2];
+        long ans = 0;
+        pointer1 = 0;
+        pointer2 = skill.length - 1;
+        while (pointer1 <= pointer2) {
+            ans += skill[pointer1] * skill[pointer2];
             pointer1++;
             pointer2--;
         }

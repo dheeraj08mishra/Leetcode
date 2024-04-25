73ce459ddc4a630ef49febb291a05eba3c872f02
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-04-25 11:10:17 +0530

    Time: 3 ms (97.26%) | Memory: 44.1 MB (98.28%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,12 +1,20 @@
 class Solution {
     public int[] minOperations(String boxes) {
         int[] ans = new int[boxes.length()];
-        for (int i = 0; i < boxes.length(); i++) {
-            for (int j = 0; j < boxes.length(); j++) {
-                if (boxes.charAt(j) != '0' && i != j) {
-                    ans[i] = ans[i] + Math.abs(i - j);
-                }
-            }
+        int[] leftPrefix = new int[boxes.length()];
+        int[] rightPrefix = new int[boxes.length()];
+        int c = boxes.charAt(0) - '0';
+        for (int i = 1; i < boxes.length(); i++) {
+            leftPrefix[i] = leftPrefix[i - 1] + c;
+            c += boxes.charAt(i) - '0';
+        }
+        c = boxes.charAt(boxes.length() - 1) - '0';
+        for (int i = boxes.length() - 2; i >= 0; i--) {
+            rightPrefix[i] = rightPrefix[i + 1] + c;
+            c += boxes.charAt(i) - '0';
+        }
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = rightPrefix[i] + leftPrefix[i];
         }
         return ans;
     }

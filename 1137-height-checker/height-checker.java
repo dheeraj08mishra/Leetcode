0b7c5ff0be6009eee94c23e8fb6ed4261ee821fc
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-04-26 22:40:31 +0530

    Time: 2 ms (89.18%) | Memory: 41 MB (81.65%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,26 +1,31 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < heights.length; i++) {
-            if (heights[i] > max) {
-                max = heights[i];
-            }
+        int [] array = new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            array[i] = heights[i];
         }
-        int[] countArray = new int[max + 1];
-        for (int i = 0; i < heights.length; i++) {
-            countArray[heights[i]]++;
-        }
-        for (int i = 1; i < countArray.length; i++) {
-            countArray[i] = countArray[i - 1] + countArray[i];
-        }
-        int[] output = new int[heights.length];
-        for (int i = 0; i < heights.length; i++) {
-            output[countArray[heights[i]] - 1] = heights[i];
-            countArray[heights[i]]--;
-        }
+        Arrays.sort(array);
+        // int max = Integer.MIN_VALUE;
+        // for (int i = 0; i < heights.length; i++) {
+        //     if (heights[i] > max) {
+        //         max = heights[i];
+        //     }
+        // }
+        // int[] countArray = new int[max + 1];
+        // for (int i = 0; i < heights.length; i++) {
+        //     countArray[heights[i]]++;
+        // }
+        // for (int i = 1; i < countArray.length; i++) {
+        //     countArray[i] = countArray[i - 1] + countArray[i];
+        // }
+        // int[] output = new int[heights.length];
+        // for (int i = 0; i < heights.length; i++) {
+        //     output[countArray[heights[i]] - 1] = heights[i];
+        //     countArray[heights[i]]--;
+        // }
         int notMatchCount=0;
         for(int i=0;i<heights.length;i++){
-            if(heights[i]!=output[i]){
+            if(heights[i]!=array[i]){
                 notMatchCount++;
             }
         }

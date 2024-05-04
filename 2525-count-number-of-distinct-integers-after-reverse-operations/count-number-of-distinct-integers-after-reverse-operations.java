7ff2bf81467e4baf30df083c6bb8300688375463
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-05-04 14:28:06 +0530

    Time: 111 ms (13.46%) | Memory: 62.5 MB (87.71%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
@@ -1,25 +1,10 @@
 class Solution {
     public int countDistinctIntegers(int[] nums) {
-        int ans[] = new int[2 * nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            ans[i] = nums[i];
-            ans[nums.length + i] = reverseCurrent(nums[i]);
-        }
         HashSet<Integer> hs = new HashSet<>();
-        for (int i = 0; i < ans.length; i++) {
-            hs.add(ans[i]);
+        for (int i = 0; i < nums.length; i++) {
+            hs.add(nums[i]);
+            hs.add(Integer.parseInt(new StringBuilder(Integer.toString(nums[i])).reverse().toString()));
         }
         return hs.size();
-    }
-
-    private static int reverseCurrent(int num) {
-        String current = Integer.toString(num);
-        StringBuilder sb = new StringBuilder(current);
-        for (int i = 0; i < current.length() / 2; i++) {
-            Character temp = current.charAt(i);
-            sb.setCharAt(i, sb.charAt(sb.length() - i - 1));
-            sb.setCharAt(sb.length() - 1 - i, temp);
-        }
-        return Integer.parseInt(sb.toString());
     }
 }

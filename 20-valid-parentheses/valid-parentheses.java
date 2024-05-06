8b4af2f1d46ea0036c8c497d87a39819758c2f06
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-05-06 22:23:32 +0530

    Time: 2 ms (55.20%) | Memory: 41.3 MB (74.72%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,43 +1,69 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        // Stack<Character> stack = new Stack<>();
+        // for (int i = 0; i < s.length(); i++) {
+        // if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
+        // stack.push(s.charAt(i));
+        // } else {
+        // if (stack.isEmpty()) {
+        // if (s.charAt(i) == '}' || s.charAt(i) == ')' || s.charAt(i) == ']') {
+        // return false;
+        // }
+        // }
+        // if (s.charAt(i) == '}') {
+        // if (stack.peek() == '{') {
+        // stack.pop();
+        // } else {
+        // stack.push(s.charAt(i));
+        // }
+        // }
+        // if (s.charAt(i) == ')') {
+        // if (stack.peek() == '(') {
+        // stack.pop();
+        // } else {
+        // stack.push(s.charAt(i));
+        // }
+        // }
+        // if (s.charAt(i) == ']') {
+        // if (stack.peek() == '[') {
+        // stack.pop();
+        // } else {
+        // stack.push(s.charAt(i));
+        // }
+        // }
+        // }
+        // }
+        // if (!stack.isEmpty()) {
+        // return false;
+        // } else {
+        // return true;
+        // }
+
+        if (s.charAt(0) == '}' || s.charAt(0) == ')' || s.charAt(0) == ']')
+            return false;
+        Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
-                stack.push(s.charAt(i));
+            if (st.isEmpty()) {
+                // if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
+                    st.push(s.charAt(i));
+                // }
             } else {
-                if (stack.isEmpty()) {
-                    if (s.charAt(i) == '}' || s.charAt(i) == ')' || s.charAt(i) == ']') {
-                        return false;
-                    }
-                }
-                if (s.charAt(i) == '}') {
-                    if (stack.peek() == '{') {
-                        stack.pop();
-                    } else {
-                        stack.push(s.charAt(i));
-                    }
-                }
-                if (s.charAt(i) == ')') {
-                    if (stack.peek() == '(') {
-                        stack.pop();
-                    } else {
-                        stack.push(s.charAt(i));
-                    }
-                }
-                if (s.charAt(i) == ']') {
-                    if (stack.peek() == '[') {
-                        stack.pop();
-                    } else {
-                        stack.push(s.charAt(i));
-                    }
+                if (s.charAt(i) == ']' && st.peek() == '[') {
+                    st.pop();
+                } else if (s.charAt(i) == '}' && st.peek() == '{') {
+                    st.pop();
+                } else if (s.charAt(i) == ')' && st.peek() == '(') {
+                    st.pop();
+                } else {
+                    st.push(s.charAt(i));
                 }
             }
+
         }
-        if (!stack.isEmpty()) {
-            return false;
-        } else {
+        if (st.isEmpty())
             return true;
-        }
+        else
+            return false;
 
     }
 }

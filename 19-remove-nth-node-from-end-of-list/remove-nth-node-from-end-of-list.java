31846336690f64d575328efa3e3c552f65416a00
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-05-22 23:34:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (66.80%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,31 +1,44 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-         ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode slow = dummy;
-        ListNode fast = dummy;
-
-        for (int i = 0; i <= n; i++) {
-            fast = fast.next;
+        ListNode previous = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
         }
-        while (fast != null) {
-            slow = slow.next;
-            fast = fast.next;
+        ListNode reversedHead = previous;
+
+        if (n == 1) {
+            reversedHead = reversedHead.next;
+        } else {
+            ListNode temp = reversedHead;
+            for (int i = 1; i < n - 1; i++) {
+                temp = temp.next;
+            }
+            if (temp.next != null) {
+                temp.next = temp.next.next;
+            }
         }
-
-        slow.next = slow.next.next;
-
-        return dummy.next;
-        
+        previous = null;
+        current = reversedHead;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
     }
 }

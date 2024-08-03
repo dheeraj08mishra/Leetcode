ebaaa1fd00f4911dfcd8c3a81c9bbd0042eb9c51
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-08-03 21:52:08 +0530

    Time: 3 ms (85.96%) | Memory: 44.2 MB (42.83%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,26 +1,40 @@
+// class Solution {
+
+//     public boolean canBeEqual(int[] target, int[] arr) {
+//         HashMap<Integer, Integer> hm_target = new HashMap<>();
+//         for (int i = 0; i < target.length; i++) {
+//             if (hm_target.containsKey(target[i])) {
+//                 hm_target.put(target[i], hm_target.get(target[i]) + 1);
+//             } else {
+//                 hm_target.put(target[i], 1);
+//             }
+//         }
+//         for (int i = 0; i < arr.length; i++) {
+//             if (hm_target.containsKey(arr[i])) {
+//                 hm_target.put(arr[i], hm_target.get(arr[i]) - 1);
+//                 if (hm_target.get(arr[i]) == 0) {
+//                     hm_target.remove(arr[i]);
+//                 }
+//             } else {
+//                 return false;
+//             }
+//         }
+//         if (hm_target.isEmpty()) {
+//             return true;
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        HashMap<Integer, Integer> hm_target = new HashMap<>();
+        Arrays.sort(target);
+        Arrays.sort(arr);
         for (int i = 0; i < target.length; i++) {
-            if (hm_target.containsKey(target[i])) {
-                hm_target.put(target[i], hm_target.get(target[i]) + 1);
-            } else {
-                hm_target.put(target[i], 1);
-            }
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (hm_target.containsKey(arr[i])) {
-                hm_target.put(arr[i], hm_target.get(arr[i]) - 1);
-                if (hm_target.get(arr[i]) == 0) {
-                    hm_target.remove(arr[i]);
-                }
-            } else {
+            if (target[i] != arr[i]) {
                 return false;
             }
         }
-        if (hm_target.isEmpty()) {
-            return true;
-        }
-        return false;
+        return true;
     }
 }
